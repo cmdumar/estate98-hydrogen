b8f162525349af142ea1548eb65a7d071926b5f9
Zaffer Ali <87249334+zafferali@users.noreply.github.com>
2023-02-27 20:04:51 +0530

    Update Journey.client.jsx

--- src/components/sections/Journey.client.jsx
+++ src/components/sections/Journey.client.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unknown-property */
 /* eslint-disable eslint-comments/disable-enable-pair */
 /* eslint-disable prettier/prettier */
 import React, {useState} from 'react'
@@ -72,19 +73,17 @@ export function Journey({story}) {
         <div className="h-screen w-screen fixed -mt-24 z-50"> 
             <div className="h-screen w-screen absolute bg-black opacity-70 -z-30"></div> {/* overlay */}
             <div className='max-w-[1200px] mx-auto mt-[60px] h-[85vh] relative'> 
-                <Image className='absolute right-0 left-0 mx-auto top-[-40px] cursor-pointer' onClick={story} src="https://cdn.shopify.com/s/files/1/0712/2793/2978/files/icons8-close-50.png?v=1676567817" alt="close-icon" width={32} height={32}/> 
+                {/* <svg className='absolute right-0 left-0 mx-auto top-[-40px] cursor-pointer hover:fill-theme' xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0,0,256,256" width="50px" height="50px" fill-rule="nonzero"><g fill="#ffffff" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style="mix-blend-mode: normal"><g transform="scale(5.12,5.12)"><path d="M7.71875,6.28125l-1.4375,1.4375l17.28125,17.28125l-17.28125,17.28125l1.4375,1.4375l17.28125,-17.28125l17.28125,17.28125l1.4375,-1.4375l-17.28125,-17.28125l17.28125,-17.28125l-1.4375,-1.4375l-17.28125,17.28125z"/></g></g><script xmlns=""/></svg> */}
+                <Image className='absolute right-0 left-0 mx-auto top-[-40px] cursor-pointer hover:fill-theme' onClick={story} src="https://cdn.shopify.com/s/files/1/0712/2793/2978/files/icons8-close.svg?v=1677503674" alt="close-icon" width={32} height={32}/> 
                 <div className='flex gap-6 absolute m-auto right-0 bottom-[-40px]'>
                     {
-                    counter > 0 ? 
-                        // <Image onClick={previousHandler} className="w-8 cursor-pointer" src="https://cdn.shopify.com/s/files/1/0712/2793/2978/files/iconL.png?v=1676626450" width={48} height={32}/>
+                    counter > 0 ?              
                         <button onClick={previousHandler} className=' text-white font-trixie text-xl transition-all hover:text-theme hover:scale-125 duration-300'>Back</button>
                         : <div className='w-8 h-8 bg-transparent'> </div>
                     }
-                    {/* <Image className="w-8 cursor-pointer" src="https://cdn.shopify.com/s/files/1/0712/2793/2978/files/iconL.png?v=1676626450" width={48} height={32}/>  */}
+                    
                     {
                     counter < data.length-1 ?
-                       
-                    // <Image onClick = {nextHandler} className="w-8 cursor-pointer" src="https://cdn.shopify.com/s/files/1/0712/2793/2978/files/iconG.png?v=1676626450" width={48} height={48}/> 
                     <button onClick = {nextHandler} className=' text-white font-trixie text-xl transition-all hover:text-theme hover:scale-125 duration-300'>Next</button>
                         : null
                     }
